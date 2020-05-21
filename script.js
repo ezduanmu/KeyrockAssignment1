f8fa9211b90bdd4dd9c7b1457cd14e4f8c2ec627
Eric Duanmu <ezduanmu@gmail.com>
2020-05-21 03:23:00 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -307,7 +307,7 @@ function executionSummaries()
 function summaryToText(s)
 {
     // s.custodian.replace('\r','') fixes the error where there is an extra \r character from custodian cell
-    return "" + s.ticker + "," + s.side + "," + s.quantity + "," + s.avgPrice + "," + s.custodian.replace('\r','') + "," + s.broker + "\r\n";
+    return "" + s.ticker + "," + s.side + "," + s.quantity + "," + s.avgPrice.toFixed(2) + "," + s.custodian.replace('\r','') + "," + s.broker + "\r\n";
 }
 
 /*                                                                          */
